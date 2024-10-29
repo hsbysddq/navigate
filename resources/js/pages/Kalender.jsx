f6312ae75d836bde573b7ede3fe8209b48398a8e
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2024-10-29 09:30:04 +0700

    feat(calender): menambahkan halaman kalender baru

---
+++ resources/js/pages/Kalender.jsx
@@ -0,0 +1,9 @@
+export default function Kalender() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-400 font-bold text-center">
+                Kalender Pendidikan
+            </h1>
+        </div>
+    );
+}
