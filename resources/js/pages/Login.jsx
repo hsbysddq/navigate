07274166317c4051afbdc948f53075ac4fe37fbd
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2024-10-29 09:29:31 +0700

    feat(login): menambahkan halaman login baru

---
+++ resources/js/pages/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-400 font-bold text-center">
+                Login
+            </h1>
+        </div>
+    );
+}
