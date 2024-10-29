a6224312190d0ba888593b9d1b61ba361e211f97
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2024-10-29 09:31:26 +0700

    feat(comparison): menambahkan komponen perbandingan baru

---
+++ resources/js/pages/Perbandingan.jsx
@@ -0,0 +1,9 @@
+export default function Perbandingan() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-400 font-bold text-center">
+                Perbandingan
+            </h1>
+        </div>
+    );
+}
