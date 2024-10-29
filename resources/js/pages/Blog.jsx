6d934e91d1f5234532e4e6f919c43092c94cb3bf
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2024-10-29 09:32:05 +0700

    feat(blog): menambahkan halaman blog baru

---
+++ resources/js/pages/Blog.jsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-400 font-bold text-center">
+                Blog
+            </h1>
+        </div>
+    );
+}
