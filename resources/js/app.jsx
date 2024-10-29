8c7e921f0dfbf36fc6cdb10368dc45aaa7e5ff8a
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2024-10-29 09:21:06 +0700

    feat(footer): menambahkan komponen footer baru

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,12 +3,12 @@ import "../css/app.css";
 
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
-import Layout from "@/Layouts/Layout";
+import Layout from "@/layouts/Layout";
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
-        let page = pages[`./Pages/${name}.jsx`];
+        const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+        let page = pages[`./pages/${name}.jsx`];
         page.default.layout =
             page.default.layout || ((page) => <Layout children={page} />);
         return page;
