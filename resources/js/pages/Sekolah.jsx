b11663d527f4bfeb582515167d11d995e13f55d4
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2024-10-29 09:28:58 +0700

    feat(school): menambahkan halaman sekolah baru

---
+++ resources/js/pages/Sekolah.jsx
@@ -0,0 +1,9 @@
+export default function Sekolah() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-400 font-bold text-center">
+                Daftar Sekolah
+            </h1>
+        </div>
+    );
+}
